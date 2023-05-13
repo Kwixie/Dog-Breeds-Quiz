bb07070e54a34cf52cd8c12af247458772e1522b
Emelie Köhler <100089053+Kwixie@users.noreply.github.com>
2023-05-13 22:19:46 +0200

    Update practice.js

--- practice.js
+++ practice.js
@@ -11,6 +11,7 @@ let dog = {};
 const answersArray = [ansOp1, ansOp2, ansOp3, ansOp4];
 let correctOpt = 1;
 let haveClicked = false;
+let dogName = "";
 
 ansOp1.addEventListener("click", checkResult);
 ansOp2.addEventListener("click", checkResult);
@@ -34,13 +35,20 @@ const fetchData = async (searchTerm) => {
   }
 };
 
-async function nextDog() {
+const getRandomDogName = () => {
   let randIndex = Math.floor(Math.random() * dogBreeds.length);
   while (prevDogs.some((idx) => idx === randIndex)) {
     randIndex = Math.floor(Math.random() * dogBreeds.length);
+    console.log(randIndex);
   }
   prevDogs.push(randIndex);
-  dog = await fetchData(dogBreeds[randIndex]);
+  dogName = dogBreeds[randIndex];
+  console.log(dogName);
+};
+
+async function nextDog() {
+  getRandomDogName();
+  dog = await fetchData(dogName);
 
   haveClicked = false;
 
@@ -57,11 +65,21 @@ async function nextDog() {
     "dogImg"
   ).src = `https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`;
 
-  const answerArray = [];
-  answerArray.push(dogBreeds[Math.floor(Math.random() * dogBreeds.length)]);
-  answerArray.push(dogBreeds[Math.floor(Math.random() * dogBreeds.length)]);
-  answerArray.push(dogBreeds[Math.floor(Math.random() * dogBreeds.length)]);
-  answerArray.push(dogBreeds[randIndex]);
+  let randOp1 = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+  while (randOp1 === dogName) {
+    randOp1 = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+  }
+  let randOp2 = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+  while (randOp2 === dogName || randOp2 === randOp1) {
+    randOp2 = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+  }
+  let randOp3 = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+  while (randOp3 === dogName || randOp3 === randOp2 || randOp3 === randOp2) {
+    randOp3 = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+  }
+
+  let answerArray = [dogName, randOp1, randOp2, randOp3];
+
   console.log(answerArray);
   shuffle(answerArray);
 
